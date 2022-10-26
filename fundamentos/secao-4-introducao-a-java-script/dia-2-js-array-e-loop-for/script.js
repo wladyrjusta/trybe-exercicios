861e07d6c661015c5006eb3a76f2cd1180b3b8be
wladyrjusta <wladyr.justa@gmail.com>
2022-10-26 12:15:27 -0300

    Realizados exercícios seção-4 dia-3

--- fundamentos/secao-4-introducao-a-java-script/dia-2-js-array-e-loop-for/script.js
+++ fundamentos/secao-4-introducao-a-java-script/dia-2-js-array-e-loop-for/script.js
@@ -16,6 +16,41 @@ if (media > 20) {
     console.log('Valor menor ou igual a 20!');
 }
 
+const valorMax = numbers.reduce(function(maiorNum, menorNum) {
+    return maiorNum > menorNum ? maiorNum : menorNum;
+});
+console.log(valorMax);
+
+
+const valorMin = numbers.reduce(function(menorNum, maiorNum) {
+    return menorNum < maiorNum ? menorNum : maiorNum;
+});
+console.log(valorMin);
+
+let impares = 0;
+let pares = 0;
+
+ for (let indexNumbers = 0; indexNumbers < numbers.length; indexNumbers += 1) {
+    if (numbers[indexNumbers] % 2 !== 0) {
+        impares += 1;
+    } else if (numbers[indexNumbers] % 2 == 0) {
+        pares += 1;
+    } else {
+        console.log('Nenhum número ímpar');
+    }
+}
+console.log('Números ímpares neste Array:' + ' ' + impares);
+console.log("Números pares neste Array:" + ' ' + pares);
+
+let newArray = Array.from(Array(26).keys());
+console.log(newArray);
+
+for (let indexNewArray = 0; indexNewArray < newArray.length; indexNewArray += 1) {
+    console.log(newArray[indexNewArray] / 2);
+}
+
+
+
 
 
 
